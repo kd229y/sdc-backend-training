861bb30c75690636638c113596237496f17e48f4
kd229y <kevin.cs13@nycu.edu.tw>
2024-09-27 02:02:16 +0800

    init commit

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
+async def read_root():
     return {"message": "Hello World"}
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Optional[str] = None):
+    response = {
+        "item_id": item_id,
+        "name": item.name,
+        "description": item.description,
+        "price": item.price,
+        "tax": item.tax
+    }
+    
+    if q:
+        response["q"] = q 
+    
+    return response
