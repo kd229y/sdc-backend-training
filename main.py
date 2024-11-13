beee2037128200e14c0b2f5d848a245c9c4ebf40
kdy <kevin122901@gmail.com>
2024-11-13 22:47:01 +0800

    add kd hw3

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-
-
-app = FastAPI()
-
+from typing import Annotated
+from fastapi import Path, Query, FastAPI
+app=FastAPI()
 @app.get("/")
-async def read_root():
+async def empty():
     return {"message": "Hello World"}
+@app.get("/items/{item_id}")
+async def HW3_1(
+    item_id: Annotated[int, Path(ge=1, le=1000)], 
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None, 
+    sort_order: str | None = "asc"):
+    
+    results = {"item_id": item_id}
+    
+    if q:
+        results.update({"description": f"This is a sample item that matches the query {q}"})
+    else:
+        results.update({"description": "This is a sample item."})
+    
+    results.update({"sort_order": sort_order})
+    
+    return results
+
+# 2. PUT "/items/{item_id}" Endpoint
+from pydantic import BaseModel
 
 class Item(BaseModel):
     name: str
@@ -16,16 +31,16 @@ class Item(BaseModel):
     tax: float
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: Optional[str] = None):
-    response = {
-        "item_id": item_id,
-        "name": item.name,
-        "description": item.description,
-        "price": item.price,
-        "tax": item.tax
-    }
+async def HW3_2(
+    item_id: Annotated[int, Path(ge=1, le=1000)],
+    item: Item,
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None):
     
-    if q :
-        response["q"] = q 
+    results = {
+		"item_id": item_id,
+		**item.model_dump()
+	}
+    if q:
+        results.update({"q": q})
     
-    return response
+    return results
